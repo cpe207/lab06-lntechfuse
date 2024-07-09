3cbba0ed78919ec2fede714bb3f5bdd2c191f3f4
lntechfuse <techtitannattawat@gmail.com>
2024-07-09 09:06:05 +0700

    hj j

--- q1.ts
+++ q1.ts
@@ -1,26 +1,22 @@
-//นายปัชนภ รอกลึง 660612151
 
-// const axios = require("axios");
 import axios from "axios";
 
-/* assign interface/type to the function definition properly */
+
 const getUser = async (userId: number) => {
   try{
-    const resp = await axios.get("https://jsonplaceholder.typicode.com/users/"+userId);
-    return resp.data.name;
+    const idResp = await axios.get("https://jsonplaceholder.typicode.com/users/" + userId);
+    const idName = idResp.data.name;
+    return idName;
   } catch (error) {
     return "INVALID USER ID";
   }
 };
 
-//test case
 const input1 = 1;
 const input2 = 100;
 
-
-//run
 getUser(input1).then((result) => console.log(result));
 getUser(input2).then((result) => console.log(result));
 
-// module.exports = getUser;
+
 export default getUser;
